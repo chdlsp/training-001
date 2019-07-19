4a3caa06dded94749768d0c7c1b85c00ddebc396
temprstemp <53087748+temprstemp@users.noreply.github.com>
2019-07-19 22:57:47 +0900

    Initial commit

---
+++ src/main/java/com/rasol/training001/Training001Application.java
@@ -0,0 +1,13 @@
+package com.rasol.training001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Training001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Training001Application.class, args);
+    }
+
+}
